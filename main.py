459f1d00f2487c2351506dca8078bd90bc53b847
whimo <me@whimo.me>
2018-11-18 07:48:53 +0300

    Write jokes to csv

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import sys
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas
 import keras
 from peakutils import indexes
-from datetime import datetime
 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.layers import Dense, Dropout, Flatten
 
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+cascade_path = sys.argv[1]
+output_path = sys.argv[2]
 weights_path = 'weights.h5'
+
+face_cascade = cv2.CascadeClassifier(cascade_path)
 
 
 def create_model():
@@ -54,7 +56,7 @@ model.load_weights(weights_path)
 def estimate_happiness(face_image_gray):
     resized_img = cv2.resize(face_image_gray, (48, 48), interpolation = cv2.INTER_AREA)
     image = resized_img.reshape(1, 48, 48, 1) / 255.0
-    happiness = model.predict(image, batch_size=1, verbose=1)
+    happiness = model.predict(image, batch_size=1)
     return happiness[0][0], resized_img
 
 
@@ -64,10 +66,10 @@ times = []
 
 while True:
     ret, frame = video_capture.read()
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY, 1)
+    image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY, 1)
 
-    faces = faceCascade.detectMultiScale(
-        img_gray,
+    faces = face_cascade.detectMultiScale(
+        image_gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
@@ -75,7 +77,7 @@ while True:
     )
 
     for (x, y, w, h) in faces:
-        face_image_gray = img_gray[y:y + h, x:x + w]
+        face_image_gray = image_gray[y:y + h, x:x + w]
 
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         happiness, resized_grayscale = estimate_happiness(face_image_gray)
@@ -97,10 +99,16 @@ joke_indexes = indexes(states, thres=0.5, min_dist=30)
 joke_scores = states - np.median(states)
 joke_scores /= max(joke_scores)
 
-for i in joke_indexes:
+df = pandas.DataFrame({'timestamp': [], 'score': []})
+for joke_num, i in enumerate(joke_indexes):
     secs = times[i] - times[0]
     mins = secs // 60
-    print('{}:{}'.format(mins, round(secs % 60)), joke_scores[i])
+    print('{}:{}'.format(round(mins), round(secs % 60)), joke_scores[i])
+    df.loc[joke_num + 1] = [int(secs), joke_scores[i]]
+
+df['timestamp'] = df['timestamp'].astype(np.int32)
+df.to_csv(output_path, index=False)
+
 
 plt.plot(states)
 plt.show()
